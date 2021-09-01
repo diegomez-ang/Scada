7e7153bbb9a0a86c475b5ca99253bfe6f948e441
Diego Alejandro Gomez Angarita <diego.gomez@umb.edu.co>
2021-09-01 17:44:58 -0500

    juego de adivinar el numero

---
+++ Adivinar/Adivinar/Adivinar/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Dim numero As Integer
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dato
+        For i = 1 To 10
+            dato = InputBox("Ingresa un número entre 1 y 999", "Juega",, 200, 200)
+
+            If IsNumeric(dato) Then
+                If dato > numero Then
+                    intento.Text += vbCrLf & "Número ingresado: " & dato.ToString() & " El número es menor"
+                    Label3.Text = i
+                ElseIf dato < numero Then
+                    intento.Text += vbCrLf & "Número ingresado: " & dato.ToString() & " El número es mayor"
+                    Label3.Text = i
+                Else
+                    intento.Text += vbCrLf & "¡GANASTE! el número era " & numero.ToString()
+                    Label3.Text = i
+                    Exit For
+                End If
+            Else
+                If dato = "" Then Exit For
+                MsgBox("Solo Numeros, pierdes un intento")
+                Label3.Text = i
+            End If
+            If i = 10 Then
+                intento.Text += vbCrLf & "Perdiste, el número era: " & numero.ToString()
+                Exit For
+            End If
+
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Randomize()
+        numero = CInt(Int((999 * Rnd()) + 1))
+        numero_generado.Text = numero
+
+
+    End Sub
+
+    Private Sub numero_generado_Click(sender As Object, e As EventArgs) Handles numero_generado.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Adivinar/Adivinar/Adivinar/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Adivinar")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Adivinar")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("e6e81b39-def9-4392-973f-dbea421a5b0f")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
